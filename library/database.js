905cbec26cc6577291552115a5f4eb7f76269acb
erihidayat06 <erihidayat549@gmail.com>
2024-05-30 08:04:26 +0700

    Create : dotenv

--- library/database.js
+++ library/database.js
@@ -1,7 +1,8 @@
+require("dotenv/config");
 module.exports = {
   multipleStatements: true,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "db_cineplex",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 };
